1361d4423546b5938e0ef632371220fc066b2e2e
jepapt2 <j.webprogram@gmail.com>
2025-01-29 23:05:10 +0900

    ProviderScopeを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'core/routing/app_router.dart';
+import 'core/theme/app_theme.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: NCDCAssignmentApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class NCDCAssignmentApp extends StatelessWidget {
+  const NCDCAssignmentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      title: 'NCDC Assignment',
+      theme: AppTheme.lightTheme,
+      routerConfig: router,
     );
   }
 }
