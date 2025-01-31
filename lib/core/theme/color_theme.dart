5a9da0ee5406f04621cdffc23f729de44323316d
jepapt2 <j.webprogram@gmail.com>
2025-02-01 03:36:16 +0900

    ボタンテーマを追加

--- lib/core/theme/color_theme.dart
+++ lib/core/theme/color_theme.dart
@@ -13,21 +13,3 @@ class ColorTheme {
   static const Color backgroundLight = Color(0xFFF5F8FA);
   static const Color backgroundDark = Color(0xFFC8E6FA);
 }
-
-class PrimaryButtonColorTheme {
-  static const Color normal = ColorTheme.brand;
-  static const Color pressed = Color(0xFF347CAB);
-  static const Color disabled = Color.fromARGB(125, 76, 179, 248);
-}
-
-class PrimaryOutlineButtonColorTheme {
-  static const Color normal = ColorTheme.backgroundWhite;
-  static const Color pressed = Color(0xFFB3B3B3);
-  static const Color disabled = Color.fromARGB(125, 76, 179, 248);
-}
-
-class SecondaryButtonColorTheme {
-  static const Color normal = Color(0xFF8B8B8B);
-  static const Color pressed = Color(0xFF8B8B8B);
-  static const Color disabled = Color(0xFF8B8B8B);
-}
