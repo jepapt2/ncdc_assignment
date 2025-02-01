2dc5c4b6e54f096d9ad5972b26f669476fbd1988
jepapt2 <j.webprogram@gmail.com>
2025-02-02 05:00:40 +0900

    router更新

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:ncdc_assignment/features/content/screens/content_detail_screen.dart';
 import '../../features/content/views/screens/content_list_screen.dart';
 
 final router = GoRouter(
@@ -6,7 +7,18 @@ final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
+      name: 'home',
       builder: (context, state) => const ContentListScreen(),
+      routes: [
+        GoRoute(
+          path: 'content/:id',
+          name: 'contentDetail',
+          builder: (context, state) {
+            final id = int.parse(state.pathParameters['id'] ?? '0');
+            return ContentDetailScreen(contentId: id);
+          },
+        ),
+      ],
     ),
   ],
 );
